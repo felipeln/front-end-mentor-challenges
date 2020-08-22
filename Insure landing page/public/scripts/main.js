e1271fc58442e726f24ef225027e68239600fbc7
Felipeln <felipe1.laureano.backup@gmail.com>
2020-08-21 22:04:52 -0300

    fixing menu and acessibility problems

--- Insure landing page/public/scripts/main.js
+++ Insure landing page/public/scripts/main.js
@@ -8,17 +8,17 @@ function mobileMenu(){
     
     if(menuLinks.classList.contains('open')){
         menuLinks.classList.remove('open')
-        menuBtn.querySelector('img').src = '/public/assets/icon-hamburger.svg'
+        menuBtn.querySelector('img').src = '../public/assets/icon-hamburger.svg'
     }else{
         menuLinks.classList.add('open')
-        menuBtn.querySelector('img').src = '/public/assets/icon-close.svg'
+        menuBtn.querySelector('img').src = '../public/assets/icon-close.svg'
     }
 
 }
 
 function mobileLink(){
     menuLinks.classList.remove('open')
-    menuBtn.querySelector('img').src = '/public/assets/icon-hamburger.svg'
+    menuBtn.querySelector('img').src = '../public/assets/icon-hamburger.svg'
 }
 
 
